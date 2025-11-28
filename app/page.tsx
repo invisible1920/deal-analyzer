56aaf979685724db87a054043eec9cdafb7daedb
invisible1920 <johnmccauley1920@gmail.com>
2025-11-28 18:44:28 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { DealAnalyzerPage } from "@/components/deal-analyzer/DealAnalyzerPage";
 
 export default function Page() {
   return (
-    <main>
+    <main style={{ padding: "32px 16px" }}>
       <PageContainer>
         <DealAnalyzerPage />
       </PageContainer>
