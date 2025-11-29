e3daa45d6ecbdbb798b1f6a1f8d1a7c51f785bf6
invisible1920 <johnmccauley1920@gmail.com>
2025-11-29 00:06:45 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ import styles from "./page.module.css";
 
 export default function Page() {
   return (
-    <main className={styles.main}>
+    <div className={styles.main}>
       <PageContainer>
         <DealAnalyzerPage />
       </PageContainer>
-    </main>
+    </div>
   );
 }
